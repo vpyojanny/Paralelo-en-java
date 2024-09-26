ce1df80ed8ad81f093a6ff1fa93b1728da78c021
vpyojanny <102862361+vpyojanny@users.noreply.github.com>
2024-09-25 23:18:42 -0400

    Commit inicial 25/09/2024 23:18
    Commit inicial 25/09/2024 23:18

---
+++ Procesador_de_Archivos_de_Logs/src/main/java/com/mycompany/procesador_de_archivos_de_logs/Procesador_de_Archivos_de_Logs.java
@@ -0,0 +1,16 @@
+/*
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
+ */
+
+package com.mycompany.procesador_de_archivos_de_logs;
+
+/**
+ *
+ * @author yojan
+ */
+public class Procesador_de_Archivos_de_Logs {
+
+    public static void main(String[] args) {
+        System.out.println("Hello World!");
+    }
+}
